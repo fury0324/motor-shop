7600fd0fcc420a727426dd380a0fe57cc1b877a5
fury0324 <steven.antonio@dashlytix.com>
2026-08-16 23:34:09 -0400

    Test: trigger CI failure to confirm Discord notification posts

---
+++ src/_ci_agent_discord_test.js
@@ -0,0 +1,8 @@
+// Temporary file to trigger a real CI failure to confirm the new
+// Discord notification step posts to the channel. Safe to delete after
+// viewing.
+const unusedDiscordTestVariable = 'this failure is intentional'
+
+export function discordNotificationTest() {
+  return 'ok'
+}
